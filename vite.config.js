f77e9ad4a469de83eb084ec58028dd65c6e9b941
Leon Cox <leon.coxsw@gmail.com>
2025-01-24 22:36:22 +0400

    bug fix with deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  base: "/",
+  base: "/nutnet-tajam-test-project",
   plugins: [
     ViteImageOptimizer({
       jpg: {
